51345d28dae3bed9b762fb0c20809a30b83727bc
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2023-01-12 03:11:29 +0100

    Use wrapped object as a export

--- src/addons/machineAddon.cpp
+++ src/addons/machineAddon.cpp
@@ -1,34 +1,17 @@
 #include <node.h>
-#include "./machine/machineAdapter.cpp"
+#include "./machine/obj.h"
 
-using namespace v8;
-using namespace std;
-
-void runMachine(const FunctionCallbackInfo<Value> &args)
+namespace demo
 {
-    Isolate *isolate = args.GetIsolate();
 
-    // Get the first argument - Typed Array of uint8
-    Local<ArrayBuffer> contents = args[0].As<ArrayBuffer>();
-    uint8_t *ptr = (uint8_t *)contents->GetBackingStore()->Data();
-    // Get the second argument - memory size of the machine (int)
-    int length = args[1].As<Number>()->Value();
+    using v8::Local;
+    using v8::Object;
 
-    // Create MachineAdapter instance
-    MachineAdapter machine = MachineAdapter(ptr, length);
-    // Run the machine and get the result - moves
-    string result = machine.run();
+    void InitAll(Local<Object> exports)
+    {
+        MachineObj::Init(exports);
+    }
 
-    // Return the result to JS
-    args.GetReturnValue()
-        .Set(String::NewFromUtf8(
-                 isolate, result.c_str())
-                 .ToLocalChecked());
+    NODE_MODULE(NODE_GYP_MODULE_NAME, InitAll)
+
 }
-
-void Initialize(Local<Object> exports)
-{
-    NODE_SET_METHOD(exports, "run", runMachine);
-}
-
-NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
